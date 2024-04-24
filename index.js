24b4e82c1cf817f3a4b8f73c50bf9815984e05ff
Dancey Apple <d.apple.coder@proton.me>
2024-04-24 18:29:31 -0500

    I imported Data from the interwebs!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import _ from 'lodash';
 import { color, log, cyan, magenta} from "console-log-colors";
+import request from 'request';
 
 const my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
@@ -16,3 +17,8 @@ console.log(cyan(file));
 
 //ASync File Access Method:
 fs.readFile('./data.json', {encoding: 'utf-8'}, (err,f) => console.log(color.magenta(f)));
+
+request('https://jsonplaceholder.typicode.com/posts', (err, resp, body) => {
+    const parsed = JSON.parse(body);
+    parsed.forEach( (x) => console.log((x)));
+});
